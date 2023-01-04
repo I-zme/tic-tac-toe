9ab626d969962b71eba5117e1f5119b4119e2f76
I-zme <inbar.amir21@gmail.com>
2023-01-04 12:46:35 +0200

    Create base js functionality
    - there is default backup for all the choices so even if nothing is selected the game can start with both players human and 1 game playing.
    - validation as backup so even if setting the custom number of rounds to a negative number it will change to 1 round.
    -on clicking the start game, the user's choices are taken taken and the user is moved to the game.html page. HOWEVER the choices aren't kept there and the game cannot start.
    
    FIXME: currently trying to make it so when clicking the start game button user will be transferred to the game.html page which will also load the choices the user made into the game, and start said game.

--- main.js
+++ main.js
@@ -1,23 +1,49 @@
-let rounds;
 const yesPlayBtn = document.querySelector('#yes-play'); //probably unncessary
 const noPlayBtn = document.querySelector('#no-play'); //choose functionality
 const numRounds = document.querySelector('#select-rounds');
 const customRounds = document.querySelector('#cutsom-rounds');
 
-const playerXController = document.querySelector(
-  '[name="choose-controller-1"]'
+const playerXControllerBtns = document.querySelectorAll(
+  '[name="choose-controller-x"]'
 );
-const playerOController = document.querySelector(
-  '[name="choose-controller-2"]'
+const playerOControllerBtns = document.querySelectorAll(
+  '[name="choose-controller-o"]'
 );
 
 const playGame = document.querySelector('#start-game-btn');
 
 numRounds.addEventListener('focusout', () => {
   if (numRounds.value === 'custom') {
-    customRounds.toggleAttribute('disabled');
-    customRounds.addEventListener('focusout', (rounds = customRounds.value));
+    customRounds.hasAttribute('disabled')
+      ? customRounds.toggleAttribute('disabled')
+      : '';
   } else {
-    rounds = numRounds.value;
+    customRounds.hasAttribute('disabled')
+      ? ''
+      : customRounds.toggleAttribute('disabled');
   }
+});
+
+// on clicking start game
+playGame.addEventListener('click', () => {
+  let rounds =
+    numRounds.value !== 'custom'
+      ? Number(numRounds.value)
+      : customRounds.value >= 1
+      ? Number(customRounds.value)
+      : 1;
+
+  const _findCheckedRadioBtn = (btns) => {
+    return Array.from(btns).find((item) => item.checked);
+  };
+  const _getBtnValue = (btns, defaultValue) => {
+    return _findCheckedRadioBtn(btns)
+      ? _findCheckedRadioBtn(btns).value
+      : defaultValue;
+  };
+
+  let playerXController = _getBtnValue(playerXControllerBtns, 'human');
+  let playerOController = _getBtnValue(playerOControllerBtns, 'human');
+  Game.play(rounds, playerXController, playerOController);
+  location.href = './game.html';
 });
