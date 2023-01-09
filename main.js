325ab86cd3edec8512caef1643f93930202b432d
I-zme <inbar.amir21@gmail.com>
2023-01-09 06:21:25 +0200

    Style for small screen and change default settings
    CSS:
    - made the background graffitti images change size with the screen width.
    - because the select option now doesnt have the super long disabled selected option the input box is smaller which made the choose-rounds div wrap less nicely. To fix this gave the custom rounds container a min-width so that choose-rounds wraps right at small screen (with custom rounds under the select).
    - change names of images to descriptorian names so that the css is easier to understand and change.
    
    HTML:
    - change the default number of rounds to 3 and it is the one shown automatically, this is the simplest way to show the default settings to the user.
    - the custom input is only shown when user chooses the custom option, this is to make the choices less confusing for the user.
    
    JS:
    - made changes in accordance with the HTML changes

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const yesPlayBtn = document.querySelector('#yes-play'); //probably unncessary
 const noPlayBtn = document.querySelector('#no-play'); //choose functionality
 const numRounds = document.querySelector('#select-rounds');
 const customRounds = document.querySelector('#cutsom-rounds');
+const customRoundsDiv = document.querySelector('.custom-container');
 
 const playerXControllerBtns = document.querySelectorAll(
   '[name="choose-controller-x"]'
@@ -14,26 +15,27 @@ const playGame = document.querySelector('#start-game-btn');
 
 numRounds.addEventListener('focusout', () => {
   if (numRounds.value === 'custom') {
-    customRounds.hasAttribute('disabled')
-      ? customRounds.toggleAttribute('disabled')
-      : '';
+    customRoundsDiv.hasAttribute('data-visible')
+      ? ''
+      : customRoundsDiv.toggleAttribute('data-visible');
   } else {
-    if (!customRounds.hasAttribute('disabled')) {
-      customRounds.toggleAttribute('disabled');
+    if (customRoundsDiv.hasAttribute('data-visible')) {
+      customRoundsDiv.toggleAttribute('data-visible');
       customRounds.value = '';
     }
   }
 });
 
 playGame.addEventListener('click', () => {
+  const defaultNumberOfRounds = 3;
   const rounds =
     numRounds.value !== 'custom'
       ? Number(numRounds.value) === 0
-        ? 1
+        ? defaultNumberOfRounds
         : Number(numRounds.value)
       : customRounds.value >= 1
       ? Number(customRounds.value)
-      : 1;
+      : defaultNumberOfRounds;
   const _findCheckedRadioBtn = (btns) => {
     return Array.from(btns).find((item) => item.checked);
   };
